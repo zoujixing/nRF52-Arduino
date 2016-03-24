ab0047d81660862d9d6e278ecf28d623794a840c
jixing.zou <jixing.zou@redbear.hk>
2016-03-24 21:23:55 +0800

    Add pin transform.

---
+++ Arduino/hardware/RBL/RBL_nRF52832/cores/RBL_nRF52832/pin_transform.h
@@ -0,0 +1,17 @@
+
+#ifndef PIN_TRANSFORM_H_
+#define PIN_TRANSFORM_H_
+
+#include "Arduino.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint8_t Pin_nRF51822_to_Arduino(uint8_t pin);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
