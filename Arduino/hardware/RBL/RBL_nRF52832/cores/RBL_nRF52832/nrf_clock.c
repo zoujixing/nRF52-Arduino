e0393f3fad52ee1c92c84ffc084ea7655f9df1e8
jixing.zou <jixing.zou@redbear.hk>
2016-03-24 21:22:54 +0800

    Add functions for LFCLK & HFCLK's configuration.

---
+++ Arduino/hardware/RBL/RBL_nRF52832/cores/RBL_nRF52832/nrf_clock.c
@@ -0,0 +1,26 @@
+
+#include "Arduino.h"
+
+void LFCLK_init(void)
+{	
+	//LFCLK Init
+    NRF_CLOCK->LFCLKSRC             = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
+    NRF_CLOCK->EVENTS_LFCLKSTARTED  = 0;
+    NRF_CLOCK->TASKS_LFCLKSTART     = 1;
+    while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0)
+    {
+        //Do nothing.
+    }
+    NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
+}
+
+void HFCLK_init(void)
+{	
+	NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+	NRF_CLOCK->TASKS_HFCLKSTART	   = 1;
+	while(NRF_CLOCK->EVENTS_HFCLKSTARTED == 0)
+	{
+		//do nothing
+	}
+	NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+}
---
+++ Arduino/hardware/RBL/RBL_nRF52832/cores/RBL_nRF52832/nrf_clock.h
@@ -0,0 +1,16 @@
+
+#ifndef NRF_CLOCK_H_
+#define NRF_CLOCK_H_
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void LFCLK_init(void);
+void HFCLK_init(void);
+
+#ifdef __cplusplus
+} 
+#endif
+
+#endif
