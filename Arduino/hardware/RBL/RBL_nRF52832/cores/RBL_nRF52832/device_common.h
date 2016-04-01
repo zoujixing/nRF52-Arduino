8079d6e8bbf0a0dfb0877214339e834390ef9671
jixing.zou <jixing.zou@redbear.hk>
2016-04-01 11:52:34 +0800

    Add some functional files.

---
+++ Arduino/hardware/RBL/RBL_nRF52832/cores/RBL_nRF52832/device_common.h
@@ -0,0 +1,16 @@
+
+#ifndef DEVICE_COMMON_H_
+#define DEVICE_COMMON_H_
+
+#include "nrf.h"
+#include "nrf52.h"
+#include "nrf52_bitfields.h"
+#include "nrf51_to_nrf52.h"
+#include "nrf52_name_change.h"
+#include "compiler_abstraction.h"
+#include "system_nrf52.h"
+
+#include "pins_arduino.h"
+#include "pin_transform.h"
+
+#endif
