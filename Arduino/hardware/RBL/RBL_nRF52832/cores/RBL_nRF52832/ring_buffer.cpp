83b73266fd4eb456271c0128692bc406fe51a7e7
jixing.zou <jixing.zou@redbear.hk>
2016-04-01 11:50:59 +0800

    Add ring buffer.

---
+++ Arduino/hardware/RBL/RBL_nRF52832/cores/RBL_nRF52832/ring_buffer.cpp
@@ -0,0 +1,55 @@
+
+#include "ring_buffer.h"
+
+/****************************************************
+*                 Function Definitions
+****************************************************/ 
+void Ring_Buffer::store( uint8_t c)
+{
+	uint16_t i = (( head +1 ) % RING_BUFFER_MAX_SIZE);
+	
+	if( i != tail )
+	{   // Store
+		ring_buffer[head] = c;
+		head = i;
+	}
+}
+
+uint16_t Ring_Buffer::available()
+{
+    return ((RING_BUFFER_MAX_SIZE + head - tail) % RING_BUFFER_MAX_SIZE);
+}
+
+uint8_t Ring_Buffer::read(void)
+{
+    uint8_t dat;
+    
+    if(head == tail)
+        return -1;
+    
+    dat = ring_buffer[tail];
+    tail = (tail + 1) % RING_BUFFER_MAX_SIZE;
+    
+    return dat;
+}
+
+uint8_t Ring_Buffer::peek(void)
+{
+    if(head == tail)
+        return -1;   
+    
+    return ring_buffer[tail];
+}
+
+void Ring_Buffer::clear(void)
+{
+    memset(ring_buffer, 0, RING_BUFFER_MAX_SIZE);
+    head = 0;
+    tail = 0;   
+}
+
+bool Ring_Buffer::isFull(void)
+{
+    uint16_t next_index = ((head + 1) % RING_BUFFER_MAX_SIZE);
+    return (next_index == tail);
+}
---
+++ Arduino/hardware/RBL/RBL_nRF52832/cores/RBL_nRF52832/ring_buffer.h
@@ -0,0 +1,42 @@
+
+#ifndef RING_BUFFER_H_
+#define RING_BUFFER_H_
+
+#include <string.h>
+#include <stdint.h>
+
+
+/****************************************************
+*                 Micro Definitions
+****************************************************/
+
+#define RING_BUFFER_MAX_SIZE 64
+
+/****************************************************
+*                 Type Definitions
+****************************************************/ 
+
+class Ring_Buffer
+{
+private:
+	uint8_t  ring_buffer[RING_BUFFER_MAX_SIZE];
+	uint16_t head;
+	uint16_t tail;
+	
+public:
+	Ring_Buffer(void){
+        memset(ring_buffer, 0, RING_BUFFER_MAX_SIZE);
+        head = 0;
+        tail = 0;
+    }
+    
+	void     store(uint8_t c);
+    uint16_t available(void);
+    uint8_t  read(void);
+    uint8_t  peek(void);
+    void     clear(void);
+    bool     isFull(void);
+};
+
+#endif
+
