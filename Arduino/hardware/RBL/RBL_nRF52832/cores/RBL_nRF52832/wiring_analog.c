49f49241050058708cbcd4140d85088add7366dc
jixing.zou <jixing.zou@redbear.hk>
2016-04-01 11:53:07 +0800

    Add analog files.

---
+++ Arduino/hardware/RBL/RBL_nRF52832/cores/RBL_nRF52832/wiring_analog.c
@@ -0,0 +1,5 @@
+
+#include "wiring_analog.h"
+
+
+
---
+++ Arduino/hardware/RBL/RBL_nRF52832/cores/RBL_nRF52832/wiring_analog.h
@@ -0,0 +1,20 @@
+
+#ifndef WIRING_ANALOG_H_
+#define WIRING_ANALOG_H_
+
+#include <stdint.h>
+#include <string.h>
+#include "device_common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
