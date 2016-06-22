02a6b9f6243ae91084e32f9ca9a2ca359af2203b
jixing.zou <jixing.zou@redbear.hk>
2016-06-22 10:21:30 +0800

    Update board information

---
+++ Arduino/hardware/RBL/RBL_nRF52832/cores/RBL_nRF52832/nrf_lib.h
@@ -0,0 +1,29 @@
+
+#ifndef NRF_LIB_H_
+#define NRF_LIB_H_
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+#include "app_scheduler.h"
+#include "app_timer.h"
+#include "app_timer_appsh.h"
+#include "app_util.h"
+#include "app_util_platform.h"
+#include "nordic_common.h"
+#include "sdk_common.h"
+#include "sdk_errors.h"
+#include "sdk_macros.h"
+
+#include "ant_stack_handler_types.h"
+#include "app_ram_base.h"
+#include "ble_stack_handler_types.h"
+#include "softdevice_handler.h"
+#include "softdevice_handler_appsh.h"
+
+#ifdef __cplusplus
+} 
+#endif 
+
+#endif
